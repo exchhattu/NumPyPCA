a57ea60c365df26d3756d28ed2291be58c3985d6
exchhattu <rojan@live.com>
2020-03-14 19:42:32 -0400

    Setup is pushed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup 
+
+setup(name ="NumpyPCA",
+        author="Rojan",
+        author_email="rojan@live.com",
+        url="https://github.com/exchhattu/NumPyPCA.git",
+        packages = ["NumPyPCA"])
+
+
