57fdb3719d7cc50bb72d60434a708dc86665c061
exchhattu <rojan@live.com>
2020-04-19 21:45:50 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup 
+from distutils.core import setup
 
-setup(name ="PCAInterface",
-        author="Rojan",
-        author_email="rojan@live.com",
-        url="https://github.com/exchhattu/NumPyPCA.git",
-        packages = ["PCA"])
-
-
+setup(
+    name="npPCA",
+    author="Rojan",
+    author_email="rojan@live.com",
+    url="https://github.com/exchhattu/NumPyPCA.git",
+    packages=["npPCA"],
+)
