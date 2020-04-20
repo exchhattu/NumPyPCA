3c7b160f9206faff8502dfc8a52afb4f180074c9
exchhattu <rojan@live.com>
2020-04-19 21:57:05 -0400

    udpate project name

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name="npPCA",
+    name="NumPyPCA",
     author="Rojan",
     author_email="rojan@live.com",
     url="https://github.com/exchhattu/NumPyPCA.git",
-    packages=["npPCA"],
+    packages=["NumPyPCA"],
 )
