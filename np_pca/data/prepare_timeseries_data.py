2cd7e956a589e94401df37af71260560d9c4d722
exchhattu <rojan@live.com>
2020-04-22 14:39:17 -0400

    created a data directory

---
+++ np_pca/data/prepare_timeseries_data.py
@@ -0,0 +1,15 @@
+'''
+Wed 22 Apr 2020 02:29:27 PM EDT
+By: Rojan Shrestha PhD
+'''
+
+class 
+
+import MDAnalysis as mda
+from MDAnalysis.analysis import align
+from MDAnalysis.analysis.rms import rmsd
+
+def parse_trajectories(path_to_ref, path_to_mobile, path_to_setup, output_file=""):
+    
+	
+
